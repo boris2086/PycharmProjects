161cf6f4a70eeea4f7e60e88461893eb03856446
boris2086 <tzars_@gmail.com>
2017-07-02 19:55:22 +0300

    work

--- Course Softuni 05.2017/08.Figures with loops/06.Diamond.py
+++ Course Softuni 05.2017/08.Figures with loops/06.Diamond.py
@@ -1,15 +1,30 @@
 n=int(input())
 
-if n>0:
-    for i in range(n):
-        for s in range(n-i):
-            print('',end=' ')
-        for j in range((i*2)-1):
-            print('*',end='')
-        print()
-    for i in range(n,0,-1):
-         for s in range(n-i):
-             print(' ',end='')
-         for j in range (i*2-1):
-             print('*',end='')
-         print()
+for i in range(n):
+    for j in range(n-i-1):
+        print(' ',end='')
+    for j in range(i):
+        print(' *',end='')
+    for j in range(n-i):
+        print(' ',end='')
+    print()
+
+print('*',end='')
+for i in range(n-1):
+    print(' *',end='')
+print()
+
+for i in range(1,n):
+    for j in range(i-1):
+        print(' ',end='')
+    for j in range(n-i):
+        print(' *',end='')
+    for j in range(i):
+        print(' ',end='')
+    print()
+
+
+
+
+
+
